85f009a7802d66176d202ed40c8e46d47e6dd875
AdhemarMolon <162194104+AdhemarMolon@users.noreply.github.com>
2025-09-17 12:38:48 -0300

    desfazendo comentário e incluindo dependência

--- FullStack_MeuVestibulinho/next.config.js
+++ FullStack_MeuVestibulinho/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import("next").NextConfig} */
 const config = {
   experimental: {
-    // reactCompiler: true, // desativado para não exigir o babel-plugin-react-compiler
+     reactCompiler: true, 
   },
 }
 
